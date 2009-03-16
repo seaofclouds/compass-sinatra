1b32caa7987def5b3fbde46a0688488950092e00
seaofclouds <todd@seaofclouds.com>
2009-03-16 13:17:56 -0700

    a note about stylesheet location.

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, "views"
 
-#
+# at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
 get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass(:"stylesheets/#{params[:name]}", :sass => { :load_paths => ( [ File.join(Sinatra::Application.views, 'stylesheets') ] + Compass::Frameworks::ALL.map { |f| f.stylesheets_directory } ) } )
